5a834088ce33e38da130d12fa72654e2c1174205
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-09-19 17:44:03 +0530

    add blog data in blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,35 @@
-import React from "react"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Layout from "../components/layout"
+import Layout from '../components/layout'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>My Blog</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laborum
-        est nulla voluptas exercitationem quae facilis consequuntur doloremque
-        esse maiores aliquid ipsum explicabo culpa quos, quod atque fuga, eum
-        quis.
-      </p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => (
+          <li>
+            <h2>{edge.node.frontmatter.title}</h2>
+            <p>{edge.node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }
