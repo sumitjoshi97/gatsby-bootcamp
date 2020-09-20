26449951ed0700487e6f6da5b5904bc58f1cca7a
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-09-20 15:09:39 +0530

    update footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 
+import footerStyles from './footer.module.scss'
+
 const Footer = () => {
   const data = useStaticQuery(graphql`
     {
@@ -13,7 +15,7 @@ const Footer = () => {
   `)
 
   return (
-    <footer>
+    <footer className={footerStyles.footer}>
       <p>
         Created by <span>{data.site.siteMetadata.author}</span>
       </p>
